0e6f6dc65ec0911c606fda032675e2a495fe878a
Chip <chip.elmer@gmail.com>
2019-05-06 13:51:01 -0500

    Added challenges set 4

---
+++ ChallengesWithTestsMarkVII/Business.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ChallengesWithTestsMarkVII
+{
+    public class Business
+    {
+        public string Name { get; set; }
+        public double TotalRevenue { get; set; }
+        public double TotalExpenses { get; set; }
+    }
+}
