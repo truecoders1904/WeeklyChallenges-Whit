ead24a2b9c7d344dd5334f8047590035fda588d2
Whit <whit@stroups.net>
2019-05-19 17:43:17 -0500

    finished two problems

--- ChallengesWithTestsMarkVII/ChallengesSet05.cs
+++ ChallengesWithTestsMarkVII/ChallengesSet05.cs
@@ -12,12 +12,41 @@ namespace ChallengesWithTestsMarkVII
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
-        }
+            for (int i = 0; i < businesses.Length; i++)             {                 if (businesses[i].TotalRevenue == 0 || businesses[i].TotalRevenue == null)                 {                     businesses[i].Name = "CLOSED";                 }              }         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+            int NumbersAscendingCount = 0;
+            int NumbersOutofOrderCount = 0;
+            int currentNumber = 0;
+            int nextNumber = 0;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                currentNumber = numbers[i];
+                nextNumber = numbers[i + 1];
+                if (currentNumber <= nextNumber)
+                {
+                    NumbersAscendingCount += 1;
+                }
+                else
+                {
+                    NumbersOutofOrderCount += 1;
+                }
+
+            }
+
+            if (NumbersOutofOrderCount >= 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
