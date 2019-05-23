b91bc3adce1c630372698c7ffa683951519245ad
Whit <whit@stroups.net>
2019-05-22 22:35:30 -0500

    halfwaythere

--- ChallengesWithTestsMarkVII/ChallengesSet06.cs
+++ ChallengesWithTestsMarkVII/ChallengesSet06.cs
@@ -12,13 +12,14 @@ namespace ChallengesWithTestsMarkVII
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
-        }
+            for (int i = 2; i < num; i++)             {                 if (num % i == 0)                 {                     return false;                 }             }             return true;         }
+
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
-        }
+            char[] strChar = str.ToCharArray();             int lastunique = 0;             int samecount = 0;             for (int i = 0; i < strChar.Length; i++)             {                 for (int e = 0; e < strChar.Length; e++)                 {                     if (strChar[i] == strChar[e])                     {                         samecount += 1;                     }                     else                     {                         lastunique = i;                      }
+
+                }             }             if (samecount == strChar.Length)             {                 return -1;             }             return lastunique;         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
@@ -27,7 +28,22 @@ namespace ChallengesWithTestsMarkVII
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            double[] emptyStringArray = new double[0];
+            if ( elements == null)
+            {
+                return emptyStringArray;
+            }
+                
+            List<double> everyNTH = new List<double>();             double[] elementsarray = elements.ToArray();
+            if (elementsarray.Length == 0 || n < 0)
+            {
+                return emptyStringArray;
+            }             for (int i = n; i <= elementsarray.Length; i += n)             {
+                  everyNTH.Add(elementsarray[i - 1]);             }
+
+            return everyNTH.ToArray();
         }
+
     }
+
 }
